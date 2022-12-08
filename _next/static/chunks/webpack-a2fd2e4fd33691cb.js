0292e96cb95d80b75cab2ffc421bfe24d71c3c9f
nadia3156 <nightlittle8@gmail.com>
2022-12-08 23:45:48 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-a2fd2e4fd33691cb.js
+++ _next/static/chunks/webpack-a2fd2e4fd33691cb.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "dfb945d1671677f0" + ".css";
+/******/ 			return "static/css/" + "5c2dd617046722ce" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
